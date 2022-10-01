7b5770c22498a129a54c362acc2e7ea5c1dfeac0
Guddee <guddee.n@homesfy.in>
2022-10-01 13:22:38 +0530

    custom library

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        My custom library
+      </div>
+    );
+  }
+}
+
+export default App;
