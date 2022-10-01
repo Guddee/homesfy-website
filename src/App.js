b0f590d70b2221eafc701c1a7986c0982d0827f8
Guddee59 <guddeenoudiyal5@gmail.com>
2022-10-01 13:32:31 +0530

    app created

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
+      <h2>
         My custom library
-      </div>
+      </h2>
     );
   }
 }
